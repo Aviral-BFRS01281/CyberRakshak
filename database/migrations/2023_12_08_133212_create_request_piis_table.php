6171ecf341cad03d68b484fbb1b3948691287dcd
salamuddin-kr <salamuddin@kartrocket.com>
2023-12-08 19:09:42 +0530

    Migrations

---
+++ database/migrations/2023_12_08_133212_create_request_piis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_piis', function (Blueprint $table) {
+            $table->string("request_id", 50)->index();
+            $table->tinyInteger("name");
+            $table->tinyInteger("email");
+            $table->tinyInteger("mobile");
+            $table->tinyInteger("awb");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_piis');
+    }
+};
